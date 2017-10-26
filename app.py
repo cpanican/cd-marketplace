64275866bdfdba3546320b2fde5c1adb9f34e9db
Chris Panican <chrispanican@gmail.com>
2017-10-25 21:57:20 -0400

    Add bootstrap, nav, and base files

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def compose():
 def admin():
 	return render_template("admin.html")
 
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
